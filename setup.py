f7e73aff593225b46402156e2df0537a07ce66a1
tanner <tanner@curibio.com>
2020-08-11 20:09:39 +0000

    updated classifiers

--- setup.py
+++ setup.py
@@ -26,8 +26,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation",
-        "Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Scientific/Engineering",
     ],
 )
