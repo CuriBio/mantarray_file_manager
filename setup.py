5d3dfb56672cd20f64bbac3e5126e0e6f31517ad
tanner <tanner@curibio.com>
2021-04-08 00:00:19 +0000

    bumped minor no. and updated changelog

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="mantarray_file_manager",
-    version="0.4.6",
+    version="0.4.7",
     description="Finds and opens Mantarray files.",
     url="https://github.com/CuriBio/mantarray-file-manager",
     author="Curi Bio",
